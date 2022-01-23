825666b3ec864b1555c90c4093b7b4b460891284
kondig <digonisk@gmail.com>
2022-01-24 00:30:21 +0200

    [0.2] useReducer

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,14 +1,122 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
-import {
-    Board,
-} from 'components';
+import { Board } from 'components';
 
 import { calculateTicTacToeWinner } from 'libraries';
 
 import './game.css';
 
-class Game extends React.Component {
+const initialState = {
+    history: [{
+        squares: Array(9).fill(null),
+    }],
+    xIsNext: true,
+    stepNumber: 0,
+    stepCoords: [null,null],
+};
+function reducer(state, action) {
+    switch (action.type) {
+        case 'newSquareClicked':
+            return {
+                ...state,
+                history: state.history.concat([{
+                    squares: action.payload.squares,
+                    stepCoords: [action.payload.col,action.payload.row],
+                }]),
+                xIsNext: !state.xIsNext,
+                stepNumber: state.history.length,
+                stepCoords: [action.payload.col,action.payload.row],
+            };
+        case 'jumpToMove':
+            return {
+                ...state,
+                stepNumber: action.payload,
+                xIsNext: (action.payload % 2) === 0,
+                indexOfMove: action.payload,
+            };
+        default:
+            throw new Error();
+    }
+}
+
+
+function Game() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleClick = (i) => {
+        const history = state.history.slice(0, state.stepNumber + 1);
+        const current = history[state.history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateTicTacToeWinner(squares) || squares[i]) {
+            return;
+        }
+        let row;
+        if      (i === 0 || i === 1 || i === 2) { row = 1 }
+        else if (i === 3 || i === 4 || i === 5) { row = 2 }
+        else if (i === 6 || i === 7 || i === 8) { row = 3 };
+        let col;
+        if      (i === 0 || i === 3 || i === 6) { col = 1 }
+        else if (i === 1 || i === 4 || i === 7) { col = 2 }
+        else if (i === 2 || i === 5 || i === 8) { col = 3 };
+        squares[i] = state.xIsNext ? 'X' : 'O';
+        dispatch({
+            type:'newSquareClicked',
+            payload: {squares, row, col}
+        });
+    };
+    const jumpTo = (step) => {
+        dispatch({
+            type:'jumpToMove',
+            payload: step
+        })
+    };
+    const history = state.history;
+    const current = history[state.stepNumber];
+    const winner = calculateTicTacToeWinner(current.squares);
+    const moves = history.map((step,move) => {
+        const desc = move ?
+            'Go to move #' + move + '   (' + history[move].stepCoords + ')' :
+            'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}
+                        style={{backgroundColor: (move === state.stepNumber) ? 'purple' : '#e3e3e3',
+                                color: (move === state.stepNumber) ? '#ffffff' : '#000000',}}>
+                    {desc}
+                </button>
+            </li>
+        )
+    });
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
+    }
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+}
+// the state mgmt logic should be managed in a separate place
+// useReducer()
+
+// const [state, dispatch] = useReducer(reducer, initialState);
+// - the initial state is the value the state is initialized width
+// - an action object describes how to update the State (has a property: type)
+// - the dispatch f is a special function that dispatches an action object
+// =============================================================================
+class GameClassComponent extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
