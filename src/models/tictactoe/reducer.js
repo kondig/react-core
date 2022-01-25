577d9bb9672f977799c58ef7b099d1ed68e11365
kondig <digonisk@gmail.com>
2022-01-25 03:09:00 +0200

    [0.3] reducer fix

--- src/models/tictactoe/reducer.js
+++ src/models/tictactoe/reducer.js
@@ -21,30 +21,31 @@ function reducer(state, action) {
             const { i } = action.payload;
             // const i = action.payload.i;
             const history = state.history.slice(0, state.stepNumber + 1);
-            const current = history[state.history.length - 1];
-            const squares = current.squares.slice();
-            console.log('actionPLAY: ',current, state.winner, squares[i])
+            const current = history[history.length - 1];
+            // const current = history[state.history.length - 1];
+            const squares = current.squares.slice();        
             if (state.winner || squares[i]) {
                 return state;
             };
+            console.log('actionPLAY: ',current, {i}, state.stepNumber)
             const row = Math.floor(i/3) + 1;
             const col = i % 3 + 1;
             squares[i] = state.xIsNext ? 'X' : 'O';
             return {
                 ...state,
                 winner: calculateTicTacToeWinner(squares),
-                history: state.history.concat([{
+                history: history.concat([{
                     squares,
                     stepCoords: [row, col],
                 }]),
                 xIsNext: !state.xIsNext,
-                stepNumber: state.history.length,
+                stepNumber: history.length,
                 stepCoords: [row, col],
             };
         };
         case JUMP_TO: {
             const { step } = action.payload;
-            console.log('actionJUMP :', state);
+            console.log('actionJUMP: ', { step });
             return {
                 ...state,
                 stepNumber: step,
